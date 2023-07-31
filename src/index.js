2ea2d607e2539193c08a1c419cd020c627349566
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-07-31 17:47:21 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let arr = Array.from(String(n), Number)
+  if (isNaN(arr[0])) {
+    arr.splice(0, 1)
+  }
+  return +arr.reverse().join('')
 }
